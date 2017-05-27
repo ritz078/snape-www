3240ae89a1bca095e6921fb227f581a5a160b15c
Ritesh Kumar <rkritesh078@gmail.com>
2017-05-27 18:11:59 +0530

    created banner

---
+++ components/Banner.js
@@ -0,0 +1,36 @@
+import React, {PureComponent} from 'react'
+import Logo from './logo'
+
+export default class Banner extends PureComponent{
+  render() {
+    const version = this.props.releases[0].name
+
+    return (
+    <div className="banner-wrapper mdl-button--colored">
+      <div className="banner-left">
+        <div className="details-wrapper">
+          <div className="logo">
+            <Logo />
+            <h4>Snape</h4>
+          </div>
+          <h2 className="snape-details">
+            A torrent client to search, stream and download torrents. Play music, view images and stream videos.
+          </h2>
+          <div className="download">
+            <a href={`https://github.com/ritz078/snape/releases/download/v${version}/snape-${version}.dmg`}
+               className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect">
+              <i className="mdi mdi-apple" />
+            </a>
+            <a href={`https://github.com/ritz078/snape/releases/download/v${version}/snape-Setup-${version}.exe`}
+               className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect">
+              <i className="mdi mdi-windows" />
+            </a>
+          </div>
+          <div className="version">Version : {version}</div>
+        </div>
+      </div>
+      <div className="banner-right" />
+    </div>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,25 @@
 import React, { PureComponent } from 'react'
+import isBrowser from 'is-in-browser'
+import stylesheet from 'styles/index.css'
+import fetch from 'isomorphic-unfetch'
+import Banner from '../components/Banner'
 
-export default class Main extends PureComponent{
-  render() {
+if (isBrowser) {
+  require('material-design-lite/material.min')
+}
+
+export default class Main extends PureComponent {
+  static async getInitialProps () {
+    const res = await fetch('https://api.github.com/repos/ritz078/snape/releases')
+    const json = await res.json()
+    return {releases: json}
+  }
+
+  render () {
     return (
-      <div className="banner-wrapper" data-id="3">
-        <style global jsx>{`
-            * {
-              margin: 0;
-              padding: 0;
-            }
-          `}</style>
-        <style jsx>{`
-          .banner-wrapper {
-            background-image: url('/static/background.jpg');
-            height: 100vh;
-            width: 100%;
-            background-size: cover;
-          }
-
-          .banner-wrapper:before {
-            content: '';
-          	position: absolute;
-          	top: 0;
-          	right: 0;
-          	bottom: 0;
-          	left: 0;
-          	background-image: linear-gradient(to bottom right,#002f4b,#000);
-          	opacity: .6;
-          }
-
-          .logo {
-            width: 120px;
-            height: 120px;
-            background-image: url('/static/logo.png');
-            background-size: cover;
-            margin: 0 auto;
-            top: 100px;
-            position: relative;
-          }
-        `}</style>
-
-        <div className="logo-wrapper">
-          <div className="logo"/>
-        </div>
+      <div>
+        <style dangerouslySetInnerHTML={{__html: stylesheet}} />
+        <Banner releases={this.props.releases}/>
       </div>
     )
   }
