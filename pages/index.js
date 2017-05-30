8bda345e6cd7625e9a46cab035ac14da8cec7ffb
Ritesh Kumar <rkritesh078@gmail.com>
2017-05-30 15:36:45 +0530

    fixes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default class Main extends PureComponent {
   static async getInitialProps () {
     const res = await fetch('https://api.github.com/repos/ritz078/snape/releases')
     const releases = await res.json()
-    return {version: releases[0].name}
+    return {version: releases && releases[0] && releases[0].name || 'Latest'}
   }
 
   trackingScript() {
